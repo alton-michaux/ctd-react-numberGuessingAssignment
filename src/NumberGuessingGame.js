6dcdb75d24060e9a6b2d4d6a3f9a91cf088086dc
alton-michaux <alton.michaux@yahoo.com>
2023-01-25 21:31:40 -0500

    converted NumberGuessingGame.js class component into a functional component

--- src/NumberGuessingGame.js
+++ src/NumberGuessingGame.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import GuessControl from "./GuessControl";
 import GuessMessage from "./GuessMessage";
 import GameOver from "./GameOver";
@@ -12,6 +12,52 @@ function getRandomNumber() {
 }
 
 const MAX_ATTEMPTS = 5;
+
+// Functional component
+
+const NumberGuessingGameToo = () => {
+  const [numberToGuess, setNumberToGuess] = useState(getRandomNumber())
+  const [numberOfGuesses, setNUmberOfGuesses] = useState(0)
+  const [latestGuess, setLatestGuess] = useState(null)
+
+
+  function handleGuess(guess) {
+    setLatestGuess(guess)
+    setNUmberOfGuesses(numberOfGuesses + 1)
+  };
+
+  function handleReset() {
+    setNumberToGuess(getRandomNumber())
+    setNUmberOfGuesses(0)
+    setLatestGuess(null)
+  }
+
+  const isCorrectGuess = latestGuess === numberToGuess
+
+  const isGameOver = isCorrectGuess || numberOfGuesses === MAX_ATTEMPTS
+
+  return (
+    <div>
+      <h2>I'm thinking of a number from 1 to 100.</h2>
+      <h2>
+        Can you guess the number I am thinking of in {MAX_ATTEMPTS} tries?
+      </h2>
+      <GuessControl onGuess={handleGuess} />
+      {isGameOver && (
+        <GameOver hasWon={isCorrectGuess} onReset={handleReset} />
+      )}
+      {!isGameOver && (
+        <GuessMessage
+          guess={latestGuess}
+          numberToGuess={numberToGuess}
+          numberOfGuesses={numberOfGuesses}
+        />
+      )}
+    </div>
+  )
+}
+
+// Class component
 
 class NumberGuessingGame extends Component {
   constructor(props) {
@@ -74,4 +120,4 @@ class NumberGuessingGame extends Component {
   }
 }
 
-export default NumberGuessingGame;
+export default NumberGuessingGameToo;
